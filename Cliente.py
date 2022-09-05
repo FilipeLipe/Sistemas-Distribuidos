dc059482ec80dccfa4abecec1cf7cccc0d12d4ef
FilipeLipe <62721945+FilipeLipe@users.noreply.github.com>
2022-09-05 17:16:32 -0300

    Atualização projeto

--- Cliente.py
+++ Cliente.py
@@ -1,17 +1,78 @@
-import socket, sys, time
+import socket, sys, time, hashlib, random
+from threading import Thread
 
 HOST = '127.0.0.1'
 PORT = 20000
 BUFFER_SIZE = 1024
 
+#VARIAVEIS GLOBAIS
+idHash = ""
+
+
+def configurarMaquina(s):
+    global idHash
+    #Inicia a thread
+    #newHash = Thread(target=gerarNovaHash, args=(s,))
+    #newHash.start()   
+    time.sleep(2)
+
+    #Envia o metodo para configurar a maquina (Metodo|IpMaquina|idHash)
+    s.send(('configMaquina|'+ HOST +'|'+ idHash).encode())
+
+    #Resposta do Servidor
+    data = s.recv(BUFFER_SIZE)
+    respostaServidor = repr(data)
+    print(respostaServidor)
+
+
+
+def gerarNovaHash(s):
+    global idHash
+    i = 0
+    while(True):
+        #Gera o Hash
+        idHashAntiga = idHash
+        id = str(random.randint(10,100))
+        idHash = hash(id)
+
+        #Caso seja o primeiro não faz o update
+        if i != 0:
+            s.send(('updateIdHash|'+idHashAntiga+'|'+idHash).encode())
+
+        #Resposta do Servidor
+        #data = s.recv(BUFFER_SIZE)
+        #respostaServidor = repr(data)
+        #print(respostaServidor)
+        #if(respostaServidor == 'hashAtualizado'):
+        print('------ # ------\nHash atualizada com sucesso\nNova Hash: '+ str(idHash) +'\n------ # ------')
+        #else:
+        #    print('Problemas ao atualizar Hash!!')
+         #   idHash = idHashAntiga
+
+        #Gira o loop
+        i += 1
+        time.sleep(60)
+
 def main(argv):
+    global idHash
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            
+            #Conecta o cliente ao servidor
             s.connect((HOST, PORT))
             print("Servidor executando!")
 
-            texto = 'Teste !'
-            s.send(texto.encode())  # texto.encode - converte a string para bytes
+            #Configura a maquina
+            configurarMaquina(s)
+            
+            while(True):
+                #Envia para o servidor
+                #s.send(texto.encode())
+
+                desconect = input("Deseja desconectar ?\nS ou N -> ")
+                if(desconect == 'S'):
+                    s.close()
+                    break
 
     except Exception as error:
         print("Exceção - Programa será encerrado!")
--- Servidor.py
+++ Servidor.py
@@ -1,9 +1,46 @@
+from posixpath import split
 import socket, sys
+import array as ary
 from threading import Thread
 
 HOST = '127.0.0.1'
 PORT = 20000
 BUFFER_SIZE = 1024
+
+#['ipMaquina','idHash']
+maquinas = []
+
+
+def updateMaquina(clientsocket,idHashAntiga, idHash):
+    global maquinas
+    print("Antes ->", maquinas)
+    for maq in maquinas:
+        if(maq[1] == idHashAntiga):
+            maq[1] = idHash
+            clientsocket.send(("hashAtualizado").encode())
+            break
+    print("Depois ->", maquinas)
+
+
+def addMaquina(clientsocket, ipMaquina, idHash):
+    try:
+        maquinas.index(ipMaquina)
+    except:
+        maquinas.append([ipMaquina,idHash])
+        print('Nova maquina adicionada com sucesso!')
+        print(maquinas)
+        clientsocket.send(("Maquina adicionada com sucesso!").encode())
+
+
+def validaMetodos(clientsocket, mensagemCliente):
+    requisicao = mensagemCliente.split('|')
+
+    if(requisicao[0] == 'updateIdHash'):
+        updateMaquina(clientsocket,requisicao[1],requisicao[2])
+    elif(requisicao[0] == 'configMaquina'):
+        addMaquina(clientsocket, requisicao[1],requisicao[2])
+        
+        
 
 
 def on_new_client(clientsocket,addr):
@@ -12,8 +49,13 @@ def on_new_client(clientsocket,addr):
             data = clientsocket.recv(BUFFER_SIZE)
             if not data:
                 break
-            texto_recebido = data.decode('utf-8')
-            print('Cliente -> {}\nPorta -> {}\nMensagem -> {}'.format(addr[0], addr[1], texto_recebido))        
+            mensagemCliente = data.decode('utf-8')
+            print('------ # ------\nCliente -> {}\nPorta -> {}\nMensagem -> {}'.format(addr[0], addr[1], mensagemCliente))    
+            #Padrão da mensagem que vai chegar {Nome Maquina Cliente | Ip Cliente | valor a Enviar | Nome Maquina Destino}
+
+            validaMetodos(clientsocket,mensagemCliente)
+            #clientsocket.send(data)
+
         except Exception as error:
             print("Erro na conexão com o cliente!!")
             return
@@ -26,7 +68,7 @@ def main(argv):
             while True:
                 server_socket.listen()
                 clientsocket, addr = server_socket.accept()
-                print('Conectado ao cliente -> ', addr)
+                print('------ # ------\n\nConectado ao cliente -> ', addr,'\n')
                 t = Thread(target=on_new_client, args=(clientsocket,addr))
                 t.start()   
     except Exception as error:
